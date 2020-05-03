c52484aff9b95c141428fa05db56a0ff0335cd05
EC2 Default User <ec2-user@ip-172-31-24-61.us-west-2.compute.internal>
2020-05-03 05:22:33 +0000

    Created a DB on Heroku

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ app.set('view engine', 'ejs');
 
 /* Configure MySQL DBMS */
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'q',
-    password: 'q',
-    database: 'quotes_db'
+    host: 'us-cdbr-east-06.cleardb.net',
+    user: 'b2a968485b2e2b',
+    password: '2285bad0',
+    database: 'heroku_f758c5b0e9a8469'
 });
 connection.connect();
 
@@ -21,6 +21,19 @@ connection.connect();
 app.get("/", async function(req, res){
     res.render("index");
 });
+
+// function dbConnection(){
+
+//   let conn = mysql.createConnection({
+//                  host: "us-cdbr-east-06.cleardb.net",
+//                  user: "b2a968485b2e2b",
+//              password: "2285bad0",
+//              database: "heroku_f758c5b0e9a8469"
+//       });
+
+// return conn;
+
+// }
 
 //starting server
 app.listen(process.env.PORT, process.env.IP, function(){
