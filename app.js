6d7e610fcf75bfc7675dab9c3fe472d67aa6f566
EC2 Default User <ec2-user@ip-172-31-24-61.us-west-2.compute.internal>
2020-05-03 07:25:37 +0000

    testing connectivity

--- app.js
+++ app.js
@@ -7,33 +7,48 @@ const app = express();
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 +app.engine('ejs', require('ejs').__express);
+app.use(express.urlencoded()); //use to parse data sent using the POST method
 
 /* Configure MySQL DBMS */
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+//     host: 'us-cdbr-east-06.cleardb.net',
+//     user: 'b2a968485b2e2b',
+//     password: '2285bad0',
+//     database: 'heroku_f758c5b0e9a8469'
+// });
+// connection.connect();
+
+const pool = mysql.createPool({
     host: 'us-cdbr-east-06.cleardb.net',
     user: 'b2a968485b2e2b',
     password: '2285bad0',
     database: 'heroku_f758c5b0e9a8469'
 });
-connection.connect();
+
+// ... later
+pool.query('select 1 + 1', (err, rows) => { /* */ });
 
 //routes
 app.get("/", async function(req, res){
-    res.render("index");
+    let villagerList = await getVillagers();  
+    res.render("index", {"villagerList":villagerList});
 });
 
-// function dbConnection(){
+//functions
+function getVillagers(){
+    return new Promise(function(resolve, reject){
+           let sql = `SELECT *
+                        FROM villagers
+                        ORDER BY villagerName`;
+           pool.query(sql, function (err, rows, fields) {
+              if (err) throw err;
+              resolve(rows);
+           });
+        
+    });//promise 
+}
 
-//   let conn = mysql.createConnection({
-//                  host: "us-cdbr-east-06.cleardb.net",
-//                  user: "b2a968485b2e2b",
-//              password: "2285bad0",
-//              database: "heroku_f758c5b0e9a8469"
-//       });
 
-// return conn;
-
-// }
 
 //starting server
 app.listen(process.env.PORT, process.env.IP, function(){
